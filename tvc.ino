4dbc2eeb011a95724a4b24f5ddb46d1f40b447bb
rogerchen12 <roger.h.chen12@gmail.com>
2024-03-23 20:38:06 -0500

    pid

--- tvc.ino
+++ tvc.ino
@@ -2,8 +2,6 @@
 #include <Adafruit_MPU6050.h>
 #include <SD.h>
 
-
-
 Adafruit_BMP3XX bmp;
 
 Adafruit_MPU6050 mpu;
@@ -19,8 +17,7 @@ void setup()
   Serial.begin(115200);
 
   while (!Serial)
-    delay(10); 
-
+    delay(10);
 
   if (!SD.begin(10))
   {
@@ -30,11 +27,9 @@ void setup()
   }
   SD.remove(F("test.txt"));
 
-  
   if (!bmp.begin_I2C())
-  { 
+  {
     Serial.println(F("Could not find a valid BMP3 sensor, check wiring!"));
-    
   }
 
   bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
@@ -42,53 +37,49 @@ void setup()
   bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
   bmp.setOutputDataRate(BMP3_ODR_50_HZ);
 
-
-
   if (!mpu.begin())
   {
     Serial.println(F("Failed to find MPU6050 chip"));
-   
   }
 
   mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
 
-
   Serial.println(F("initialization done."));
 
   delay(100);
 }
 
-
 void loop()
 {
+
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
 
-
- 
-  
-  if (! bmp.performReading()) {
+  if (!bmp.performReading())
+  {
     Serial.println(F("Failed to perform reading"));
   }
-  
+
   if (state == 0 & numLoop > 0)
   {
     GroundLevelPressure = bmp.pressure / 100.0;
     state++;
   }
 
+/*
+  WRITING TO SD CARD CODE
   if (state > 0)
   {
     myFile = SD.open(F("test.txt"), FILE_WRITE);
     if (myFile)
     {
       myFile.print(F("Time: "));
-      myFile.print(((numLoop-1) * 100)/1000);
+      myFile.print(((numLoop - 1) * 100) / 1000);
       myFile.print(F(" s Alt: "));
       myFile.print(bmp.readAltitude(GroundLevelPressure));
-      //myFile.print(" m Ax: " + a.acceleration.x +  +  a.acceleration.y + " m/s^2 Az: " +  a.acceleration.z + " m/s^2");
+      // myFile.print(" m Ax: " + a.acceleration.x +  +  a.acceleration.y + " m/s^2 Az: " +  a.acceleration.z + " m/s^2");
       myFile.print(F(" m Ax: "));
       myFile.print(a.acceleration.x);
       myFile.print(F(" m/s^2 Ay: "));
@@ -97,29 +88,37 @@ void loop()
       myFile.print(a.acceleration.z);
       myFile.println(F(" m/s^2"));
       myFile.close();
-
     }
     else
     {
       Serial.println(F("error opening test.txt"));
     }
-
   }
-
+ 
   numLoop++;
+ */
+  
+
+  //integrate readings twice to get attitude
+
+  //call PID function twice (for pitch and yaw)
+
+  //send output to servo
 
 
-  //milliseconds
+  // milliseconds
   delay(100);
 }
 
-double getPID(double angle){
-  //IMPLEMENT PID.PY here with P = 0.5, I = 0.1, D = 0.5 and +/-15 degrees max and min
+double getPID(double angle)
+{
+  // IMPLEMENT PID.PY here with P = 0.5, I = 0.1, D = 0.5 and +/-15 degrees max and min
 
-  return ;
+  return;
 }
 
-double getServoOutput(double actuatorCom){
+double getServoOutput(double actuatorCom)
+{
   //
-  return ;
+  return;
 }
