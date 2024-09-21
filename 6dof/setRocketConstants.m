e8c027846a1d91e2ac81a817c1dab2cc49e9cd1d
rogerchen12 <roger.h.chen12@gmail.com>
2024-09-20 23:33:20 -0500

    g

--- 6dof/setRocketConstants.m
+++ 6dof/setRocketConstants.m
@@ -12,13 +12,32 @@ wetmass = 0.3; %kg
 drymass = 0.25;
 currentmass = 0.3;
 
+diameter = 0.4;
+S = (pi/4)*0.4^2;
+
+
 motor = table2array(readtable('motor.txt'));
+
+
+%GNC
+qBody2IMU = [1 0 0 0];
+time_step = 0.01;  % consider running sim and 'controls' at different hz??
 
 %motorcurve = interp1(motor(:, 1), motor(:, 2), 0:0.01:2.25)
 %plot(0:0.01:2.25, motorcurve)
 %motorcurve = interp1(motor(:, 1), motor(:, 2), 2.43)
 
-save("params.mat")
+
+
+
+%constants
+
+earth_radius = 6371852; %m ASSUMING LAUNCH IS AT SPACEPORT
+earth_mass = 5.9722 * 10 ^ (24); %kg
+G = 6.6743 * 10 ^ (-11); 
+
+
+save("params.mat");
 
 %{
 hws = get_param(bdroot, 'modelworkspace');
